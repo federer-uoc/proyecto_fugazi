7e8e6d3f1694f0910aafd723759851d56693f00a
federer-uoc <federer@uoc.edu>
2026-03-24 22:27:06 +0100

    version 4

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 // federer@uoc.edu ok
 package main
-
 import (
+	"fmt"
 	"net/http"
+	"os"
 )
-
 func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/index.html")
+		hostname, _ := os.Hostname()
+		fmt.Fprintf(w, `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Producto 1 - Go</title>
+		</head>
+		<body>
+			<h1>Soy alumno de la UOC</h1>
+			<p>Servidor: %s</p>
+			<img src="/static/imagen.jpg" width="360">
+		</body>
+		</html>
+		`, hostname)
 	})
-
 	http.ListenAndServe(":8080", nil)
 }
